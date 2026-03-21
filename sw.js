2492b90b47fb6b732cb4b4bc2797feeeea05ce3e
madmantim <90958600+madmantim@users.noreply.github.com>
2026-03-21 15:47:17 +1100

    Fix poster/product alignment, add anime avatars

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sugoi-deck-v3';
+const CACHE = 'sugoi-deck-v4';
 const ASSETS = ['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png', 'apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
