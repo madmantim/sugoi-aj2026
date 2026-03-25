aef7bc4d0419640b85180784bef6c69f3ffabc8e
madmantim <90958600+madmantim@users.noreply.github.com>
2026-03-25 18:41:21 +1100

    v12: reorder slides, HE release dates, dual contact emails

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sugoi-deck-v11';
+const CACHE = 'sugoi-deck-v12';
 const ASSETS = ['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png', 'apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
