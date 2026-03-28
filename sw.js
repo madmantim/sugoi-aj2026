2e75103023146c705e94db2ee8ae8542231af15d
madmantim <90958600+madmantim@users.noreply.github.com>
2026-03-29 06:47:24 +0900

    v15: enable pinch-to-zoom for ad-hoc zooming on posters/products

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sugoi-deck-v14';
+const CACHE = 'sugoi-deck-v15';
 const ASSETS = ['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png', 'apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
