0bf512ea772fe7988c37561d6a0bca83e4418db5
madmantim <90958600+madmantim@users.noreply.github.com>
2026-03-29 06:51:05 +0900

    v16: revert to zoom-locked, cleaner presentation feel

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sugoi-deck-v15';
+const CACHE = 'sugoi-deck-v16';
 const ASSETS = ['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png', 'apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
